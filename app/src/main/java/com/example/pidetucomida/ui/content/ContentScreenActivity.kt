5af6b4660101e699f880ac46920c8f5c8ca32686
Daniel Stanciu <marti90@hotmail.es>
2023-04-29 17:56:04 +0200

    Design and bottom onClickListener

---
+++ app/src/main/java/com/example/pidetucomida/ui/content/ContentScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pidetucomida.ui.content
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pidetucomida.R
+
+class ContentScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_screen)
+    }
+}
---
+++ app/src/main/java/com/example/pidetucomida/ui/login/MainActivityViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.pidetucomida.ui.login
+
+class MainActivityViewModel {
+}
