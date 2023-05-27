ca01a64d274cffebd8487c906d3115b50487c38d
Daniel Stanciu <marti90@hotmail.es>
2023-05-28 01:37:02 +0200

    save a product more than once

--- app/src/main/java/com/example/pidetucomida/data/database/entities/ProductEntity.kt
+++ app/src/main/java/com/example/pidetucomida/data/database/entities/ProductEntity.kt
@@ -5,12 +5,14 @@ import androidx.room.PrimaryKey
 import com.example.pidetucomida.model.Product
 
 @Entity
-data class ProductEntity (val img: ByteArray,
-                          @PrimaryKey(autoGenerate = false)
-                          val idProducto: Int,
-                          val nombre: String,
-                          val precio: Double,
-                          val tipo: String,
-                          val descripcion: String)
+data class ProductEntity(
+    @PrimaryKey(autoGenerate = true) var id: Int,
+    val img: ByteArray,
+    val idProducto: Int,
+    val nombre: String,
+    val precio: Double,
+    val tipo: String,
+    val descripcion: String
+)
 
-fun Product.toDomain()= ProductEntity(img,idProducto,nombre,precio,tipo,descripcion)
+fun Product.toDomain() = ProductEntity(id,img, idProducto, nombre, precio, tipo, descripcion)
--- app/src/main/java/com/example/pidetucomida/model/Product.kt
+++ app/src/main/java/com/example/pidetucomida/model/Product.kt
@@ -3,11 +3,12 @@ package com.example.pidetucomida.model
 import com.example.pidetucomida.model.product.ProductResponse
 
 
-data class Product (val img: ByteArray,
+data class Product (var id:Int,
+                    val img: ByteArray,
                     val idProducto: Int,
                     val nombre: String,
                     val precio: Double,
                     val tipo: String,
                     val descripcion: String)
 
-fun ProductResponse.toDomain()= Product(img,idProducto,nombre,precio,tipo,descripcion)
+fun ProductResponse.toDomain()= Product(0,img,idProducto,nombre,precio,tipo,descripcion)
