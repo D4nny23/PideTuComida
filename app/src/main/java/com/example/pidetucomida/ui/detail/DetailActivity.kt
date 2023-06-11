701743d72d2e6d55a507a1797169610b81f602e8
Daniel Stanciu <marti90@hotmail.es>
2023-06-11 17:02:39 +0200

    Clear activities behind detail

--- app/src/main/java/com/example/pidetucomida/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/pidetucomida/ui/detail/DetailActivity.kt
@@ -98,8 +98,8 @@ class DetailActivity : AppCompatActivity() {
             if (isSaved){
                 Toast.makeText(this, "Producto guardado correctamente", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, ContentScreenActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
-                finish()
             }else{
                 Toast.makeText(this, "Producto no guardado", Toast.LENGTH_SHORT).show()
             }
